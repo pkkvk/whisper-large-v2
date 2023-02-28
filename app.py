2164673dbe561f9423f7fbc01533eebc78c96c81
Karthikeyan <anuppankulam@gmail.com>
2023-02-28 12:02:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from io import BytesIO
 def init():
     global model
     
-    model = whisper.load_model("base")
+    model = whisper.load_model("large-v2")
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
@@ -26,7 +26,19 @@ def inference(model_inputs:dict) -> dict:
         file.write(mp3Bytes.getbuffer())
     
     # Run the model
-    result = model.transcribe("input.mp3")
+    result = model.transcribe("input.mp3",
+                             temperature = model_inputs.get('temperature',0.0),
+                             initial_prompt= model_inputs.get('initial_prompt',None),
+                             compression_ratio_threshold= model_inputs.get('compression_ratio_threshold',2.4),
+                             logprob_threshold= model_inputs.get('compression_ratio_threshold',-1.0),
+                             no_speech_threshold= model_inputs.get('compression_ratio_threshold',0.6),
+                             condition_on_previous_text.get('condition_on_previous_text',True), 
+                             language= model_inputs.get('language',None), 
+                             fp16= model_inputs.get('fp16',False),
+                             task= model_inputs.get('task','transcribe'), 
+                             beam_size= model_inputs.get('beam_size',None),
+                             patience= model_inputs.get('patience',None),
+                             prompt= model_inputs.get('best_of',None))
     output = {"text":result["text"]}
     os.remove("input.mp3")
     # Return the results as a dictionary
