ebc4632b14fd007359999917ec09cdd817a98b9e
Karthikeyan <anuppankulam@gmail.com>
2023-02-28 12:30:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,15 +30,15 @@ def inference(model_inputs:dict) -> dict:
                              temperature = model_inputs.get('temperature',0.0),
                              initial_prompt= model_inputs.get('initial_prompt',None),
                              compression_ratio_threshold= model_inputs.get('compression_ratio_threshold',2.4),
-                             logprob_threshold= model_inputs.get('compression_ratio_threshold',-1.0),
-                             no_speech_threshold= model_inputs.get('compression_ratio_threshold',0.6),
-                             condition_on_previous_text.get('condition_on_previous_text',True), 
+                             logprob_threshold= model_inputs.get('logprob_threshold',-1.0),
+                             no_speech_threshold= model_inputs.get('no_speech_threshold',0.6),
+                             condition_on_previous_text= model_inputs.get('condition_on_previous_text',True), 
                              language= model_inputs.get('language',None), 
                              fp16= model_inputs.get('fp16',False),
                              task= model_inputs.get('task','transcribe'), 
                              beam_size= model_inputs.get('beam_size',None),
                              patience= model_inputs.get('patience',None),
-                             prompt= model_inputs.get('best_of',None))
+                             prompt= model_inputs.get('prompt',None))
     output = {"text":result["text"]}
     os.remove("input.mp3")
     # Return the results as a dictionary
